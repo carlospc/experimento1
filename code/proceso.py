50ad16f6e0ebf72671557df5197e3787ea662e20
carlospc <carlospc@carlospc.com>
2019-10-17 05:37:21 -0500

    españoL

--- code/proceso.py
+++ code/proceso.py
@@ -2,12 +2,12 @@
 
 from urllib.request import urlopen
 txt = urlopen("https://raw.githubusercontent.com/carlospc/experimento1/master/txt/cien-anios-de-soledad.txt").read().decode("utf-8", errors="replace").split("\n")
-print("Our dataset contains {} scripts".format(len(txt)))
+print("El dataset contiene {} líneas".format(len(txt)))
 # ADVANCED_CHANGEME -- You can change this to load any text file 
 # You want it to be one line of plain text for every script.  Extra
 # annotations like [John:] or *starts coughing* make learning more difficult.
 everything = set([w for s in txt for w in s.split()])
-print("and {} lexical types".format(len(everything)))
+print("y {} tipos léxicos".format(len(everything)))
 
 # STEP 1.2
 
